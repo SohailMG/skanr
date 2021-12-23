66caf7ed7b4c63f750414e9ef78bfa1ba380b2de
Sohail Gsais <Gsais.sohail9@gmail.com>
2021-12-23 16:09:58 +0000

    added logo to login

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { View, Text, SafeAreaView, Image } from "react-native";
-import tw from 'tailwind-rn'
-import SvgAnimation from "../components/SvgAnimation";
+import tw from "tailwind-rn";
 
 const HomeScreen = () => {
   return (
-    <View style={tw('flex-1')}>
+    <View style={tw("flex-1")}>
       {/* Top scanner */}
-      <View style={tw('flex-1 absolute top-0 right-0')}>
+      <View style={tw("flex-1 absolute top-0 right-0")}>
         <Image
           source={require("../assets/scanner.png")}
           width={100}
@@ -16,6 +15,12 @@ const HomeScreen = () => {
         />
       </View>
       {/* Middle section */}
+      <View style={[tw("ml-10 mr-10"), { marginVertical: "80%" }]}>
+        <Text style={tw("text-3xl font-semibold text-gray-800")}>
+          Scan restaurant banner or search by name
+        </Text>
+      </View>
+      {/* TODO: Search bar */}
     </View>
   );
 };
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/core";
 import React, { useLayoutEffect } from "react";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 import {
   Button,
   Image,
@@ -33,10 +33,11 @@ const LoginScreen = () => {
       {/* Welcome text */}
       <View style={tw("flex-1 items-start ml-10")}>
         <View style={tw("flex flex-row items-center")}>
-          <Text style={tw("font-bold text-4xl text-green-800 mr-2")}>
-            Scannitor
-          </Text>
-          <AntDesign name="scan1" size={50} color="black" />
+          <Image
+            source={require("../assets/appLogo.png")}
+            resizeMode="contain"
+            style={[tw(""), { width: 200, height: 100 }]}
+          ></Image>
         </View>
         <Text style={tw("font-light text-xl text-gray-400 mt-2")}>
           Using AI to give you the details you need about a restaurant before
@@ -46,9 +47,7 @@ const LoginScreen = () => {
       {/* Sign in button */}
       <View
         style={[
-          tw(
-            "flex-1 absolute p-2 bottom-40 w-52 bg-white shadow-4xl rounded-full "
-          ),
+          tw("flex-1 absolute p-2 bottom-40 w-52 bg-white  rounded-full "),
           {
             marginHorizontal: "25%",
             shadowColor: "#171717",
@@ -65,6 +64,27 @@ const LoginScreen = () => {
           <AntDesign name="google" size={24} color="black" />
           <Text style={tw("ml-2 text-center font-semibold")}>
             Continue with google
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* create account button */}
+      <View
+        style={[
+          tw("flex-1 absolute p-2 w-52 bg-white  rounded-full "),
+          {
+            marginHorizontal: "25%",
+            shadowColor: "#171717",
+            bottom: 100,
+            shadowOffset: { width: -2, height: 4 },
+            shadowOpacity: 0.2,
+            shadowRadius: 3,
+          },
+        ]}
+      >
+        <TouchableOpacity style={tw("flex flex-row items-center")}>
+          <Ionicons name="ios-create" size={24} color="green" />
+          <Text style={tw("ml-2 text-center font-semibold")}>
+            Create Account
           </Text>
         </TouchableOpacity>
       </View>
