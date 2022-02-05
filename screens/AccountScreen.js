8cd54f7fbcf0356d046f82c30cebb3b3f565bc53
Sohail Gsais <Gsais.sohail9@gmail.com>
2022-02-05 18:28:30 +0000

    updated account page

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
-import { View, Text, Button, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useAuth from "../hooks/useAuth";
 import tw from "tailwind-rn";
 import { Image } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import { Checkbox } from "react-native-ui-lib";
-
+import { AntDesign, MaterialIcons, Entypo } from "@expo/vector-icons";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  Incubator,
+  RadioButton,
+} from "react-native-ui-lib";
+import { useRef } from "react";
+const { TextField } = Incubator;
 const catagories = {
   13377: "vegan",
   13191: "Halal",
@@ -18,74 +31,178 @@ const AccountScreen = () => {
   const { logout, user } = useAuth();
   const [halalChecked, setHalalChecked] = useState(false);
   const [veganChecked, setveganChecked] = useState(false);
+  const [activeForms, setActiveForms] = useState(false);
+
+  const inputRef = useRef();
+  const editForms = () => {
+    setActiveForms(true);
+    inputRef.current.focus();
+  };
+
   return (
-    <SafeAreaView style={{ backgroundColor: "#EEEAD8", flex: 1 }}>
-      {/* Header */}
-      <View style={tw("items-center")}>
-        <Text style={tw("font-semibold text-md ")}>Account</Text>
+    <View style={{ backgroundColor: "#FFFF", flex: 1 }}>
+      {/* Top view */}
+      <View
+        style={[
+          tw("flex h-1/3 relative"),
+          {
+            backgroundColor: "#EEEAD8",
+            borderBottomLeftRadius: 100,
+            borderBottomRightRadius: 100,
+          },
+          styles.boxShadow,
+        ]}
+      >
+        {/* top header */}
+        <SafeAreaView style={tw("flex flex-row justify-between m-4")}>
+          <TouchableOpacity
+            disabled={true}
+            onPress={editForms}
+            style={[
+              tw(
+                "flex flex-row items-center justify-center p-2 bg-white rounded-full "
+              ),
+              { width: 90 },
+              styles.boxShadow,
+            ]}
+          >
+            <Text style={tw("font-semibold mr-2")}>Edit</Text>
+            <AntDesign name="edit" size={20} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={logout}
+            style={[
+              tw(
+                "flex justify-center flex-row items-center p-2 bg-white rounded-full "
+              ),
+              { width: 90 },
+              styles.boxShadow,
+            ]}
+          >
+            <Text style={tw("font-bold  mr-2")}>Logout</Text>
+            <MaterialIcons name="logout" size={20} color="red" />
+          </TouchableOpacity>
+        </SafeAreaView>
+        {/* Profile image */}
+        <View
+          style={[
+            tw("absolute bottom-0 -mb-12 self-center rounded-full"),
+            styles.boxShadow,
+            { borderColor: "#464545", borderWidth: 2 },
+          ]}
+        >
+          <Image
+            style={[tw("w-40 h-40  bg-white rounded-full")]}
+            source={{ uri: user?.photoURL }}
+          />
+        </View>
       </View>
 
-      {/* Account details */}
-      <View style={[tw("items-center mt-20 flex")]}>
-        <Image
-          style={tw("h-20 w-20 rounded-full")}
-          source={{ uri: user?.photoURL }}
-        />
-        <View style={tw("self-start m-2 p-2 flex")}>
-          <Text style={tw("mb-2 mt-2")}>Name</Text>
-          <Text
-            style={tw("text-lg mb-2  text-gray-400 bg-white p-2 rounded-lg")}
-          >
-            {user?.displayName}
-          </Text>
-          <Text style={tw("mb-2 mt-2")}>Email</Text>
-          <Text
-            style={tw("text-lg  text-lg text-gray-400 bg-white p-2 rounded-lg")}
-          >
-            {user?.email}
-          </Text>
-        </View>
-        {/* Prefrence options */}
-        <View style={tw("self-start ml-4")}>
-          <View style={tw("flex flex-row items-center")}>
-            <Text style={tw("text-lg text-gray-500 font-light mt-4 mb-4 mr-2")}>
-              Set your dietary preference
-            </Text>
-            <AntDesign name="setting" size={20} color="black" />
-          </View>
-          <View
-            style={tw("flex flex-row items-center bg-white rounded-md p-4")}
-          >
-            <Checkbox
-              color="green"
-              label="Halal"
-              value={halalChecked}
-              onValueChange={() => setHalalChecked(!halalChecked)}
-              containerStyle={{}}
-            />
-            <Checkbox
-              color="green"
-              label="Vegetarian"
-              value={veganChecked}
-              onValueChange={() => setveganChecked(!veganChecked)}
-              containerStyle={{ marginHorizontal: 10 }}
-            />
-          </View>
-        </View>
-        <TouchableOpacity
-          onPress={logout}
-          style={tw(
-            "w-64 flex flex-row mt-10  items-center justify-center p-3 rounded-xl  bg-red-400"
-          )}
+      {/* Middle view */}
+      <View style={tw("flex mt-10 h-40")}>
+        <Text
+          style={[tw("mt-4 self-center"), { fontSize: 30, fontWeight: "600" }]}
         >
-          <Text style={tw("text-center font-semibold mr-4 text-white text-xl")}>
-            Sign out
-          </Text>
-          <AntDesign name="logout" size={20} color="white" />
+          {user?.displayName}
+        </Text>
+        <View style={[tw("flex flex-row items-center"), styles.input]}>
+          <MaterialIcons
+            style={tw("mr-2")}
+            name="email"
+            size={24}
+            color="black"
+          />
+          <TextInput
+            onBlur={() => setActiveForms(false)}
+            ref={inputRef}
+            editable={activeForms}
+            value={user?.email}
+          />
+        </View>
+        <View style={[tw("flex flex-row items-center"), styles.input]}>
+          <Entypo style={tw("mr-2")} name="lock" size={24} color="black" />
+          <TextInput editable={activeForms} value={"********"} />
+        </View>
+      </View>
+
+      {/* Prefrences */}
+      <View style={tw("flex flex-row items-center")}>
+        <TouchableOpacity
+          onPress={() => setHalalChecked(!halalChecked)}
+          style={[
+            tw(
+              `flex m-4 mt-10 ${
+                halalChecked ? "bg-gray-800" : "bg-gray-100"
+              } items-center rounded-md`
+            ),
+            {
+              flexDirection: "col",
+              justifyContent: "center",
+              width: 110,
+              height: 110,
+            },
+            styles.boxShadow,
+          ]}
+        >
+          <Image
+            source={require("../assets/HalalSign.png")}
+            style={tw("h-20 w-20")}
+            resizeMode="contain"
+          />
+          <RadioButton color="gray" selected={halalChecked} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setveganChecked(!veganChecked)}
+          style={[
+            tw(
+              `flex m-4 mt-10 ${
+                veganChecked ? "bg-gray-800" : "bg-gray-100"
+              } items-center rounded-md`
+            ),
+            {
+              flexDirection: "col",
+              justifyContent: "center",
+              width: 110,
+              height: 110,
+            },
+            styles.boxShadow,
+          ]}
+        >
+          <Image
+            source={require("../assets/VegetarianMark.png")}
+            style={tw("h-20 w-20")}
+            resizeMode="contain"
+          />
+          <RadioButton color="gray" selected={veganChecked} />
         </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 export default AccountScreen;
+const styles = StyleSheet.create({
+  boxShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  buttonDisabled: {
+    backgroundColor: "#ccc",
+    color: "#999",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    backgroundColor: "#EEEEEE",
+    width: "50%",
+    borderRadius: 10,
+  },
+});
